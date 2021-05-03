c29b984a81ac186c38148fd13dc7b7350bb07b8b
OlgaPetrova3103 <73943461+OlgaPetrova3103@users.noreply.github.com>
2021-05-03 21:33:54 +0300

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <deque>
+#include <vector>
+#include <map>
+#include <iostream>
+#include <fstream>
+#include"textgen.h"
+
+using namespace std;
+int main()
+{
+    string line;
+    string text = "";
+    ifstream in("text.txt");
+    if (in.is_open())
+    {
+        while (getline(in, line))
+        {
+            text = text + line + " ";
+        }
+    }
+    in.close();
+    Text gn = Text(text, 2, 1000);
+    string answer = gn.getText();
+    ofstream out;
+    out.open("result.txt");
+    if (out.is_open())
+    {
+        out << answer;
+    }
+    out.close();
+}
